e8120a48942507e07270e5680ddc4b38ad9c7f24
mateusz <mateschm@gmail.com>
2024-02-21 19:37:16 +0000

    Add UI to the game

--- game.js
+++ game.js
@@ -1,6 +1,22 @@
 let computerScore = 0;
 let playerScore = 0;
 
+const computerInfo = document.createElement('div');
+computerInfo.style.color = 'white';
+computerInfo.textContent = `Computer score: ${computerScore}`;
+document.body.insertBefore(computerInfo, document.body.firstChild);
+
+const playerInfo = document.createElement('div');
+playerInfo.style.color = 'white';
+playerInfo.textContent = `Your score: ${playerScore}`;
+document.body.insertBefore(playerInfo, document.body.firstChild);
+
+const info = document.createElement('div');
+info.style.color = 'white';
+info.textContent = `Choose your weapon!`;
+document.body.insertBefore(info, document.body.firstChild);
+
+
 function getComputerChoice() {
     let array = ["rock", "paper", "scissors"];
     let randInt = randomGenerator(0, 2);
@@ -15,7 +31,7 @@ function randomGenerator(min, max) {
 }
 
 function getPlayerChoice() {
-    let choice = prompt("Please choose your weapon!");
+    let choice = rock.textContent;
     choice = choice.toLowerCase();
     while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
         choice = prompt("Choose the correct weapon!");
@@ -26,57 +42,62 @@ function getPlayerChoice() {
 
 function playRound() {
     let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
+    let playerChoice = btnText;
     if (computerChoice === playerChoice) {
-        console.log("It's a tie!")
+        info.textContent = "It's a tie!";
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win! Scissors beat paper.");
+        info.textContent = "You win! Scissors beat paper.";
         playerScore += 1;
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats scissors.")
+        info.textContent = "You lose! Rock beats scissors.";
         computerScore += 1;
     } else if (playerChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats rock.");
+        info.textContent = "You win! Paper beats rock.";
         playerScore += 1;
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beat paper.")
+        info.textContent = "You lose! Scissors beat paper.";
         computerScore += 1;
     }else if (playerChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats scissors.");
+        info.textContent = "You win! Rock beats scissors.";
         playerScore += 1;
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats rock.")
+        info.textContent = "You lose! Paper beats rock.";
         computerScore += 1;
     }
 }
 
 function playGame () {
-    console.log(`The score is:
-    Computer: ${computerScore}
-    Player: ${playerScore}`)
-    playRound();
-    console.log(`The score is:
-    Computer: ${computerScore}
-    Player: ${playerScore}`)
-    playRound();
-    console.log(`The score is:
-    Computer: ${computerScore}
-    Player: ${playerScore}`)
-    playRound();
-    console.log(`The score is:
-    Computer: ${computerScore}
-    Player: ${playerScore}`)
-    playRound();
-    console.log(`The score is:
-    Computer: ${computerScore}
-    Player: ${playerScore}`)
-    playRound();
-    console.log(`The score is:
-    Computer: ${computerScore}
-    Player: ${playerScore}`)
+    if ((computerScore < 5) && (playerScore < 5)) {
+        playRound();
+        computerInfo.textContent = `Computer score: ${computerScore}`;
+        playerInfo.textContent = `Your score: ${playerScore}`;
+    } else if (computerScore === 5) {
+        computerInfo.textContent = `Computer score: ${computerScore}`;
+        playerInfo.textContent = `Your score: ${playerScore}`;
+        info.textContent = `You lost :(
+                            Choose your weapon to start a new game!`;
+        computerScore = 0;
+        playerScore = 0;
+    } else {
+        computerInfo.textContent = `Computer score: ${computerScore}`;
+        playerInfo.textContent = `Your score: ${playerScore}`;
+        info.textContent = `You won :)
+                            Choose your weapon to start a new game!`;
+        computerScore = 0;
+        playerScore = 0;
+    }
 }
 
-playGame();
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function() {
+        btnText = button.textContent;
+        playGame();
+    });
+  });
+
+
 
 
 
